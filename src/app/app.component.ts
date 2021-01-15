12c411858497763137d1b9f922a8dd25955cc35a
Arturo <arthurvdwerf@hotmail.com>
2021-01-15 13:01:56 +0100

    Hide and show Div

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,7 +7,15 @@ import { Component, OnInit } from '@angular/core';
 })
 export class AppComponent implements OnInit {
 
+  //following line is the initial status of a div
+  appear: boolean = false;
+
   ngOnInit(): void {
+
+    //the following formula changes the state of a div (makes is appear)
+    this.appear = true
+
+
 
     console.log("componentIkHouVanPasta")
 
