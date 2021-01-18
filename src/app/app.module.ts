a8352de70743a298bad36eceb925ac03dc3e9201
Arturo <arthurvdwerf@hotmail.com>
2021-01-18 17:21:34 +0100

    Wikipedia template in components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,11 +3,15 @@ import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 import { WikipediaComponent } from './components/wikipedia/wikipedia.component';
+import { BodyComponent } from './components/body/body.component';
+import { InfoboxComponent } from './components/infobox/infobox.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    WikipediaComponent
+    WikipediaComponent,
+    BodyComponent,
+    InfoboxComponent
   ],
   imports: [
     BrowserModule
