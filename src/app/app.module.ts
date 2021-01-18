b34279a26078990d6bd9f239a98d8a9c465dda64
Arturo <arthurvdwerf@hotmail.com>
2021-01-18 16:26:05 +0100

    Add Wikipedia menu

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,12 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { WikipediaComponent } from './components/wikipedia/wikipedia.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    WikipediaComponent
   ],
   imports: [
     BrowserModule
